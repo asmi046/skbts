d3e6de7c12d4f0a4c06b4a37028df3a91afd5701
asmi046 <asmi046@gmail.com>
2024-11-02 02:13:05 +0300

    новая база

--- database/seeders/WorkPartSeeder.php
+++ database/seeders/WorkPartSeeder.php
@@ -28,7 +28,42 @@ class WorkPartSeeder extends Seeder
             [
                 'title' => "Колесные и ходовые элементы",
                 'services' => file_get_contents(public_path('works/hodovie.html'))
-            ]
+            ],
+
+            [
+                'title' => "Модификация и перепланировка автомобиля",
+                'services' => file_get_contents(public_path('works/plane.html'))
+            ],
+
+            [
+                'title' => "Специальные транспортные средства",
+                'services' => file_get_contents(public_path('works/spets.html'))
+            ],
+
+            [
+                'title' => "Погрузочно-разгрузочное оборудование",
+                'services' => file_get_contents(public_path('works/pogr.html'))
+            ],
+
+            [
+                'title' => "Грузовые и специализированные платформы",
+                'services' => file_get_contents(public_path('works/gruz.html'))
+            ],
+
+            [
+                'title' => "Специальное оборудование и оснастка",
+                'services' => file_get_contents(public_path('works/osn.html'))
+            ],
+
+            [
+                'title' => "Автомобили для перевозки грузов и людей",
+                'services' => file_get_contents(public_path('works/gr_l.html'))
+            ],
+
+            [
+                'title' => "Специальные услуги и доработки",
+                'services' => file_get_contents(public_path('works/sud.html'))
+            ],
         ];
 
         DB::table("work_parts")->insert($data);
