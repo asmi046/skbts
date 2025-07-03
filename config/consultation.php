97f051c737169d6435712f97c250adec6d655b01
asmi046 <asmi046@gmail.com>
2025-07-03 16:45:01 +0300

    новая почтк

--- config/consultation.php
+++ config/consultation.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'mailadresat' => env('MAIL_ADRESAT', "asmi046@gmail.com, autotuning46@yandex.ru, 89192133353@mail.ru"),
+    'mailadresat' => env('MAIL_ADRESAT', "asmi046@gmail.com, autotuning46@yandex.ru, 89192133353@mail.ru, sch@aprofi46.ru"),
 ];
